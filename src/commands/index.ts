052717a3a70c95595367d2754dcdad05ee1fa051
yamiM0NSTER <jomex50@gmail.com>
2024-01-07 10:40:03 +0100

    add command for opening sample Webview

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,12 +4,14 @@ import { openFileDialog } from './openFileDialog';
 import { selectDirectoryDialog } from './selectDirectoryDialog';
 import { showNotification } from './showNotification';
 import { showNotificationWithOptions } from './showNotificationWithOptions';
+import { openWebview } from './openWebview';
 
 const commands: { [key: string]: () => Promise<void> } = {
   'Open File Dialog': openFileDialog,
   'Select Directory Dialog': selectDirectoryDialog,
   'Show Notification': showNotification,
   'Show Notification with options': showNotificationWithOptions,
+  'Open Webview': openWebview,
 };
 
 const commandKeys = Object.keys(commands);
